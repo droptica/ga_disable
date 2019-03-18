3b0b0c524625b12739078ef5c477903f3aed4b61
Grzegorz Pietrzak <grzegorz.pietrzak@droptica.pl>
2019-03-18 09:29:30 +0100

    TAC-2 Fixed JS script plcement

--- src/Controller/CookieController.php
+++ src/Controller/CookieController.php
@@ -3,6 +3,8 @@
 namespace Drupal\ga_disable\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\PageCache\ResponsePolicy\KillSwitch;
 
 /**
  * Class CookieController.
@@ -12,14 +14,38 @@ use Drupal\Core\Controller\ControllerBase;
 class CookieController extends ControllerBase {
 
   /**
+   * @var Drupal\Core\PageCache\ResponsePolicy\KillSwitch
+   */
+  protected $killSwitch;
+
+  /**
+   * @inheritdoc
+   */
+  public static function create(ContainerInterface $container) {
+    $kill_switch = $container->get('page_cache_kill_switch');
+
+    return new static($kill_switch);
+  }
+
+  /**
+   * CookieController constructor.
+   *
+   * @param \Drupal\Core\PageCache\ResponsePolicy\KillSwitch $kill_switch
+   */
+  public function __construct(KillSwitch $kill_switch)
+  {
+    $this->killSwitch = $kill_switch;
+  }
+
+  /**
    * Sets a cookie and redirects to the frontpage.
    *
    * @return \Symfony\Component\HttpFoundation\RedirectResponse
    */
   public function setCookie() {
     setcookie("analytics_disable", TRUE, 0, '/');
-    \Drupal::messenger()->addStatus($this->t('The GA opt-out cookie has been set.'));
-    \Drupal::service('page_cache_kill_switch')->trigger();
+    $this->messenger()->addStatus($this->t('The GA opt-out cookie has been set.'));
+    $this->killSwitch->trigger();
     return $this->redirect('<front>');
   }
 
@@ -30,8 +56,8 @@ class CookieController extends ControllerBase {
    */
   public function removeCookie() {
     setcookie("analytics_disable", FALSE, time() - 3600, '/');
-    \Drupal::messenger()->addStatus($this->t('The GA opt-out cookie has been removed.'));
-    \Drupal::service('page_cache_kill_switch')->trigger();
+    $this->messenger()->addStatus($this->t('The GA opt-out cookie has been removed.'));
+    $this->killSwitch->trigger();
     return $this->redirect('<front>');
   }
 }
