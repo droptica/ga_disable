fcb2ea7247a564c44390e34f39ec7ccae8d45a71
Grzegorz Pietrzak <grzegorz.pietrzak@droptica.pl>
2019-03-19 10:08:29 +0100

    TAC-2 Fixed JS script plcement again

--- js/ga_disable.js
+++ js/ga_disable.js
@@ -7,6 +7,7 @@
  */
 
 if (document.cookie.match(/^(.*;)?\s*analytics_disable\s*=\s*[^;]+(.*)?$/)) {
-  window['ga-disable-' + drupalSettings.ga_disable.ga_id] = true;
-  console.log("Google Analytics opt-out activated.");
+  var ga_id = document.getElementById("ga_disable").getAttribute("data-id");
+  window['ga-disable-' + ga_id] = true;
+  console.log("Google Analytics opt-out activated for " + ga_id + '.');
 }
