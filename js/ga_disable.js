58e6ca33da8375ad598823f79f439393705a4847
Grzegorz Pietrzak <grzegorz.pietrzak@droptica.pl>
2019-04-01 09:22:26 +0200

    TAC-2 Added env condition

--- js/ga_disable.js
+++ js/ga_disable.js
@@ -6,7 +6,11 @@
  * should be set on early stage of page load.
  */
 
-if (document.cookie.match(/^(.*;)?\s*analytics_disable\s*=\s*[^;]+(.*)?$/)) {
+var cookie_condition = document.cookie.match(/^(.*;)?\s*analytics_disable\s*=\s*[^;]+(.*)?$/);
+var force_condition = document.getElementById("ga_disable").getAttribute("data-force") === '1';
+
+// Check all conditions.
+if (cookie_condition || force_condition) {
   var ga_id = document.getElementById("ga_disable").getAttribute("data-id");
   window['ga-disable-' + ga_id] = true;
   console.log("Google Analytics opt-out activated for " + ga_id + '.');
--- src/Form/ModuleConfigurationForm.php
+++ src/Form/ModuleConfigurationForm.php
@@ -37,6 +37,13 @@ class ModuleConfigurationForm extends ConfigFormBase {
       '#description' => $this->t('Enter the ID used by Google Analytics, like UA-1234567-8.'),
       '#default_value' => $config->get('ga_id'),
     ];
+    $form['dev_environments'] = array(
+      '#type' => 'textarea',
+      '#title' => $this->t('Dev environments'),
+      '#default_value' => $config->get('dev_environments'),
+      '#description' => $this->t('Enter the list of environments for which GA will be always disabled (in this case the cookie will be bypassed). The environment is taken from `ENV` system variable. Please put one environment in the row.'),
+      '#rows' => 5,
+    );
     $form['help'] = [
       '#type' => 'item',
       '#title' => t('Setting the cookie'),
@@ -51,6 +58,7 @@ class ModuleConfigurationForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('ga_disable.settings')
       ->set('ga_id', $form_state->getValue('ga_id'))
+      ->set('dev_environments', $form_state->getValue('dev_environments'))
       ->save();
     parent::submitForm($form, $form_state);
   }
